071bc1ad57fab003ac37e055057f277e61c5013c
psiep8 <82312159+psiep8@users.noreply.github.com>
2022-09-29 10:22:23 +0200

    Creazione navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule, Pipe} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {ButtonComponent} from "./button/button.component";
+import {FilterPipe} from "./pipes/filter.pipe";
+import {OrderPipe} from "./pipes/order.pipe";
+import {TabellaComponent} from "./tabella/tabella.component";
+import {FilterpagePipe} from "./pipes/filterpage.pipe";
+import {PaginationPipe} from "./pipes/pagination.pipe";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ButtonComponent,
+    TabellaComponent,
+    OrderPipe,
+    FilterPipe,
+    PaginationPipe,
+    FilterpagePipe,
   ],
   imports: [
     BrowserModule
@@ -15,4 +27,5 @@ import { NavbarComponent } from './navbar/navbar.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
