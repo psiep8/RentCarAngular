13803239d35c0b82598106d42bd028499563a53b
psiep8 <82312159+psiep8@users.noreply.github.com>
2022-10-03 16:36:09 +0200

    Aggiunta dati nella tabella presi tramite InMemoryDataService

--- src/app/data_components/customer-table/customer-table.component.ts
+++ src/app/data_components/customer-table/customer-table.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Customer} from "../../customer";
-import {CUSTOMERS} from "../../mock-customers";
 import {
   MyHeaders,
   MyOrder,
@@ -10,7 +9,7 @@ import {
   MyTableConfig
 } from "../../components/tabella/myclasses";
 import {CustomerService} from "../../service/customer_service/customer.service";
-import {Observable} from "rxjs";
+import {CUSTOMERS} from "../../mock-customers";
 
 @Component({
   selector: 'app-customer-table',
@@ -19,7 +18,8 @@ import {Observable} from "rxjs";
 })
 export class CustomerTableComponent implements OnInit {
 
-  customers: Customer[] = [];
+  customers: any = [];
+
   tableConfig!: MyTableConfig;
   order!: MyOrder;
   search!: MySearch;
@@ -49,18 +49,35 @@ export class CustomerTableComponent implements OnInit {
       key: "telefono",
       label: "Telefono"
     }, {
-      key: "dataNascita",
+      key: "dataDiNascita",
       label: "Data di nascita"
     }]
     this.order = {
-      defaultColumn: "id", orderType: "asc"
+      defaultColumn: "id", orderType: "desc"
     }
     this.search = {
       columns: ["id", "nome", "cognome", "email", "telefono", "dataNascita"]
     }
     this.pagination = {
-      itemPerPage: 2, itemPerPageOptions: [2, 3]
+      itemPerPage: 2, itemPerPageOptions: [2, 3, 5]
     }
+    this.actions = [{
+      icon: "https://it.seaicons.com/wp-content/uploads/2016/11/Button-Add-icon.png",
+      label: "Aggiungi nuovo utente",
+      customCssClass: "btn btn-dark",
+      buttonOnTop: true
+    }, {
+      icon: "https://static.thenounproject.com/png/1054395-200.png",
+      label: "Modifica",
+      customCssClass: "btn btn-secondary",
+      buttonOnTop: false
+    }, {
+      icon: "https://img.favpng.com/15/18/2/button-delete-key-icon-png-favpng-QyKEi5YZShJs1T6X5mdfkLUSW.jpg",
+      label: "Elimina",
+      customCssClass: "btn btn-primary",
+      buttonOnTop: false
+    }
+    ]
     this.tableConfig = {
       headers: this.headers, order: this.order, search: this.search, pagination: this.pagination, actions: this.actions
     }
@@ -69,8 +86,8 @@ export class CustomerTableComponent implements OnInit {
   }
 
   getCustomers(): void {
-    this.customers = this.customerService.getCustomers()
-    console.log('customer: ' + this.customers.toString())
+    this.customerService.getCustomers().subscribe(customers => this.customers = customers);
   }
+
 
 }
